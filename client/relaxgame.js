72b76988d1ae8a217efb9cdf2df3062131d646b4
Victoria <sivosha2006@mail.ru>
2021-07-29 22:47:41 +0300

    change organizing of relaxgame component to useState/useEffect

--- client/relaxgame.js
+++ client/relaxgame.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const RelaxGame = () => {
+  const [show, setShow] = useState(false)
+
+  const toggle = () => {
+    setShow(show)
+  }
+
+  useEffect(() => {
+    toggle()
+  }, [show])
+
   const square = new Array(5000).fill(null).map((it) => {
     return (
       <div
-        className="square w-16 h-16 bg-gray-700 m-1 shadow transition duration-100 ease-in"
+        className={`square w-4 h-4 0 m-0.5 shadow transition duration-100 ease-in ${
+          show ? 'bg-pink-700' : 'bg-gray-700'
+        }`}
         key={`it_${it}`}
+        onMouseEnter={toggle}
       />
     );
-  })
+  });
 
   return (
-    <div className="board flex items-center justify-center flex-wrap">
+    <div className="board flex items-center justify-center flex-wrap h-screen overflow-hidden">
       {square}
     </div>
   )
 }
+
 export default RelaxGame
